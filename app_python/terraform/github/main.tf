28c5c4ad0a3c4fd585fa567b8f7de558f78fcd9d
Marat Gubaidullin <m.gubaidullin@innopolis.university>
2024-11-10 00:11:04 +0300

    remove requirement PR

--- app_python/terraform/github/main.tf
+++ app_python/terraform/github/main.tf
@@ -34,12 +34,6 @@ resource "github_branch_protection" "main_branch_protection" {
     strict   = true
     contexts = []
   }
-
-  required_pull_request_reviews {
-    dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
-    required_approving_review_count = 1
-  }
 }
 
 # Output the repository URL
